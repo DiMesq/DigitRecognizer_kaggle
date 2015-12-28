d998208bffdd9590487dc5c7d136de6d112e8354
DiMesq <d.mesquita285@gmail.com>
2015-12-28 23:02:00 +0000

    Performs forward prop.

--- neural_net.py
+++ neural_net.py
@@ -1,10 +1,12 @@
+from helpers import sigmoid
+import numpy as np
+
 def neural_net(thetas, *args):
 	''' Returns the neural net cost and it's gradient for some parameter theta
 
 		theta: 1D vector, the parameters (unrolled) to the network 
 		*args: - X -> m by 784 matrix of values between 0.0 and 1.0
 		       - Y -> 1D vector of size m, the expected output for the input x
-		       - alpha -> float, learning rate
 		       - regula -> float, regularization hyper-parameter
 		       - layer_sizes -> 1D vector of ints, the layer sizes by order, 
 		       					[input layer, 
@@ -13,9 +15,10 @@ def neural_net(thetas, *args):
 
 	X = args[0]
 	Y = args[1]
-	alpha = args[2]
-	regula = args[3]
-	layer_sizes = args[4]
+	regula = args[2]
+	layer_sizes = args[3]
+
+	m = X.shape[0] 
 	n_layers = len(layer_sizes)
 
 	# roll-up parameters (weights)
@@ -28,6 +31,26 @@ def neural_net(thetas, *args):
 		list_thetas.append( np.array(thetas[k : k + l*j]).reshape(j, l))
 
 		k += l*j
+
+	###### foward prop, back prop and compute cost ######
+	cost = 0
+	
+
+	for i in range(m):
+
+		layer_outputs = [] 
+
+		activations = X[i].copy().transpose()
+
+		# foward prop
+		for Theta in list_thetas:
+
+			activations = sigmoid(Theta.dot(activations))
+			layer_outputs.append(activations)
+
+	return cost
+
+
 
 	
 		
