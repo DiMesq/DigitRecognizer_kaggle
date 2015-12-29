0ff4efc8461e9ed93441cc2e042d10756566a5b6
DiMesq <d.mesquita285@gmail.com>
2015-12-29 19:20:24 +0000

    Compute the gradient of the cost function.

--- neural_net.py
+++ neural_net.py
@@ -1,7 +1,11 @@
 from helpers import sigmoid
-import numpy as np
+import numpy as np	
 
-def neural_net(thetas, *args):
+global iter_cost
+iter_cost = []
+
+
+def net_cost_and_grad(thetas, *args):
 	''' Returns the neural net cost and it's gradient for some parameter theta
 
 		theta: 1D vector, the parameters (unrolled) to the network 
@@ -12,6 +16,7 @@ def neural_net(thetas, *args):
 		       					[input layer, 
 		       					 hidden_layer_1, (...), hidden_layer_n, 
 		       					 output_layer]'''
+	global iter_cost
 
 	X = args[0]
 	Y = args[1]
@@ -23,12 +28,14 @@ def neural_net(thetas, *args):
 
 	# roll-up parameters (weights)
 	list_thetas = []
+	list_deltas = []
 	k = 0
 	for i in range(n_layers - 1):
 		l = layer_sizes[i]
 		j = layer_sizes[i+1]
 
 		list_thetas.append( np.array(thetas[k : k + l*j]).reshape(j, l))
+		list_deltas.append( np.zeros((j, l)))
 
 		k += l*j
 
@@ -38,9 +45,8 @@ def neural_net(thetas, *args):
 
 	for i in range(m):
 
-		layer_outputs = [] 
-
-		activations = X[i].copy().transpose()
+		activations = X[i: i+1].copy().transpose()
+		layer_outputs = [activations] 
 
 		# foward prop
 		for Theta in list_thetas:
@@ -50,21 +56,64 @@ def neural_net(thetas, *args):
 
 		# add this training example cost
 		output = layer_outputs[-1]
-		cost += np.sum( Y[i] * np.log(output) + (1 - Y[i]) * np.log(1 - output))
 
+		y = [0 if k != Y[i] else 1 for k in range(10)] # make the Y(i) vector
+		y = np.array(y).reshape(10, 1)
+
+		cost += np.sum( y * np.log(output) + (1 - y) * np.log(1 - output))
+
+		# backprop to compute gradient
+		layers_errors = [] # last layer errors first and so on
+
+		layers_errors.append(layer_outputs[-1] - y)
+
+		#back propagate the errors
+		for k in range(n_layers - 2, 0, -1):
+			theta_T = list_thetas[k].transpose()
+			prev_errors = layers_errors[-1]
+			sigmoid_deriv = layer_outputs[k] * (1 - layer_outputs[k])
+
+			error = theta_T.dot(prev_errors) * sigmoid_deriv
+			layers_errors.append(error)
+
+		for k in range(n_layers - 1):
+			error = layers_errors[n_layers - 2 - k]
+			activation = layer_outputs[k].transpose()
+
+			list_deltas[k] += error.dot(activation)
+
+	D = []
+	for k in range(n_layers - 1):		
+		theta = list_thetas[k]
+		delta = list_deltas[k]
+
+		Dk = (1/m) * delta 
+		Dk[:, 1:] = Dk[:, 1:] + regula * theta[:, 1:]
+		D.append(Dk)
+
+	# unroll parameters again
+	grad_out = []
+	for Dk in D:
+		grad_out += list(Dk.flatten().reshape(-1,))
 
 	# add the regularization term to the cost 
 	thetas_cost = 0
 	for Theta in list_thetas:
 		thetas_cost += np.sum(Theta[:, 1:] ** 2) # removes the weights of the bias units
 
+	cost = (-1 / m) * cost
 	cost += (regula / (2 * m)) * thetas_cost 
 
-	return cost
+	iter_cost.append(cost)
+	return cost, np.array(grad_out)	
 
 
 
-	
-		
+
+
+
+
+
+
 		
 
