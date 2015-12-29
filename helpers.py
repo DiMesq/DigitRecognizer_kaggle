67e7f94d81ea6852abd19698173bb4c18dd811f7
DiMesq <d.mesquita285@gmail.com>
2015-12-29 22:28:42 +0000

    Creates fucntion to write the predictions to a file in the csv format.

--- helpers.py
+++ helpers.py
@@ -2,9 +2,11 @@ import csv
 import numpy as np
 import json
 
-def read_pixel_data(filename, nrows):
+def read_pixel_data(filename, nrows, flag):
 
 	with open(filename, 'rt') as csvfile:
+		''' flag: boolean, true if the data to read is the test set data (only 784 columns).
+		          Otherwise false (when reading the training data - 785 columns)'''
 
 		reader = csv.reader(csvfile)
 		next(reader) #skip header row
@@ -13,6 +15,9 @@ def read_pixel_data(filename, nrows):
 		i = 0
 		for row in reader:
 			row_ints = [int(x) for x in row]
+
+			if flag: row_ints = [1] + row_ints
+
 			data[i, :] = row_ints
 			i+=1
 
@@ -25,7 +30,7 @@ def read_json_object(filename):
 
 	return data
 
-def write_data(data, filename):
+def write_json_object(data, filename):
 	''' serializes the data as a json object in the file filename
 
 		data: list
@@ -33,6 +38,17 @@ def write_data(data, filename):
 
 	with open(filename, 'w') as f:
 		json.dump(data, f)
+
+def write_predictions(data, filename):
+
+	with open(filename, 'w') as csvfile:
+		fieldnames = ['ImageId', 'Label']
+		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+		writer.writeheader()
+
+		for i, prediction in enumerate(data):
+			writer.writerow({'ImageId': i+1, 'Label': prediction})
 
 def parse_data(array, max_value):
 	''' Changes original array
