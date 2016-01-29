1088c9814000a40be7708949d460570bfc04ce83
DiMesq <d.mesquita285@gmail.com>
2016-01-29 15:43:46 +0000

    Creates script to use the trained neural network to predict the digits in the test set.

---
+++ obj_orient_ann/main_use.py
@@ -0,0 +1,24 @@
+import json
+import helpers as h
+import settings as s
+import DigitRecognizerANN
+import numpy as np
+
+test_examples = h.read_pixels(s.TEST_DATA, s.N_TEST_EXAMPLES, s.N_PIXELS_PER_IMAGE, True, False)
+
+nn2 = DigitRecognizerANN.DigitRecognizerANN(s.LAYERS_SIZES)
+
+nn2.set_params(h.read_json_object(s.BEST_THETAS))
+
+predictions = []
+for i in range(test_examples.shape[0]):
+
+	example = test_examples[i:i+1, :].transpose()
+
+	net_output = nn2.predict(example)
+
+	digit = [k for k in range(len(net_output)) if k == np.argmax(net_output)]
+
+	predictions += digit
+
+h.write_predictions(predictions, s.MY_TEST_OUTPUT)
